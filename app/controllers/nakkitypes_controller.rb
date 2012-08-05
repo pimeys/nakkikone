d4e0e60fbb30af3d72275d9738569e25ed7bcf1f
Sami Airaksinen <samiaira@gmail.com>
2012-08-05 22:42:36 +0300

    Added first proto for nakkitype controller.

--- app/controllers/nakkitypes_controller.rb
+++ app/controllers/nakkitypes_controller.rb
@@ -2,18 +2,52 @@ class NakkitypesController < ApplicationController
   skip_before_filter :require_login
   
   def index
-    @nakkitypes = Nakkitype.where(:party_id => params[:party_id])
+    @current_party = Party.find(params[:party_id])
+    @nakkitypes = @current_party.nakkitypes
     respond_to do |format|
-      format.json { render :json => @nakit}
+      format.json { render :json => @nakkitypes}
     end
   end
 
   def update
+    @nakkitype = Nakkitype.find(params[:id])
     
+    @nakkitype.name = params[:type]
+    @nakkitype.starttime = params[:start]
+    @nakkitype.endtime = params[:end]
+    if @nakkitype.save
+      respond_to do |format|
+        format.json { render :json => @nakkitype}
+      end
+    end
   end
 
   def new
-    
+    @current_party = Party.find(params[:party_id])
+    @nakkitype = @current_party.nakkitypes.create({:name => params[:type],
+                                                    :starttime => params[:start],
+                                                    :endtime => params[:end]})
+    if @nakkitype
+      respond_to do |format|
+        format.json { render :json => @nakkitype}
+      end
+    end
+  end
+
+  def create
+    self.new
+  end
+
+  def destroy
+    @current_party = get_current_party
+    @current_party.nakkitypes.destroy(params[:id])
+    respond_to do |format|
+      format.json { render :json => {}}
+    end
+  end
+
+  def get_current_party
+    Party.find(params[:party_id]);
   end
 
 end
