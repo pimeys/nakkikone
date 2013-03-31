7709a8892bb86be9663824862822774f6f899a87
Sami Airaksinen <samiaira@gmail.com>
2013-03-31 18:25:05 +0300

    removed dublicate items from serialized JSON of parcipitants.

--- app/controllers/parcipitants_controller.rb
+++ app/controllers/parcipitants_controller.rb
@@ -7,12 +7,12 @@ class ParcipitantsController < ApplicationController
     current_party.nakkitypes.each{ |t| nakkilist += t.nakkis }
     parcipitants = nakkilist.map{ |nakki| nakki.user }
     
-    render :json => parcipitants.select{ |t| !t.nil? }, :root => false
+    render :json => parcipitants.select{ |t| !t.nil? }.uniq{ |t| t.email }, :root => false
   end
 
   def aux_index
     current_party = get_current_party
-    parcipitants_in_nakkis = current_party.aux_nakkis.select{ |t| !t.user.nil? }.map{ |nakki| nakki}
+    parcipitants_in_nakkis = current_party.aux_nakkis.select{ |t| !t.user.nil? }.uniq
     render :json => parcipitants_in_nakkis, :root => false, :each_serializer => AuxUserSerializer
   end
 
