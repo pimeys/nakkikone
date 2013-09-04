5c553993467a8612683e726d128ca9b7964de958
Sami Airaksinen <samiaira@gmail.com>
2013-09-04 21:59:29 +0300

    continuing to configure capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,18 @@
 set :application, "NakkiKone 2.0"
-set :repository,  "set your repository location here"
+set :repository, "https://github.com/EntropyRy/nakkikone.git"
+set :branch, "master"
+
+set :deploy_to, "/var/railsapps/"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
 
 role :web, "your web-server here"                          # Your HTTP server, Apache/etc
 role :app, "your app-server here"                          # This may be the same as your `Web` server
 role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 role :db,  "your slave db-server here"
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
