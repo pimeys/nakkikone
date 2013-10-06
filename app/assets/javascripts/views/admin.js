c20c809a2c7414224830254c5cc92d296eb75443
Sami Airaksinen <samiaira@gmail.com>
2013-10-06 20:41:31 +0300

    Extracted Party Editor to separate component.

--- app/assets/javascripts/views/admin.js
+++ app/assets/javascripts/views/admin.js
@@ -8,6 +8,7 @@ define([
     'components/notification-area',
     'components/users-list',
     'components/party-selector',
+    'components/party-editor',
     'moment',
     'languages',
     'bootstrapDatepicker',
@@ -15,10 +16,8 @@ define([
     'hbs!templates/admin-screen',
     'hbs!templates/nakit',
     'hbs!templates/edit-nakkis',
-    'hbs!templates/party-description',
-    'hbs!templates/party-editor-form',
     'hbs!templates/email-button'
-], function($, _, bb, collections, models, notificationArea, usersList, partySelector, moment, languages, bootstarpDP, bootstarpTP, adminScreen, nakkilist, nakkilist_edit, party_description, party_edit, emailButton) {
+], function($, _, bb, collections, models, notificationArea, usersList, partySelector, partyEditor, moment, languages, bootstarpDP, bootstarpTP, adminScreen, nakkilist, nakkilist_edit, emailButton) {
 
     //TODO fix party creation refresh for nakkitypes editor.
 
@@ -216,119 +215,6 @@ define([
 	    vent.trigger('alert', message);
 	}
     });
-    
-    var Party_Viewer = bb.View.extend({
-	events: {
-	    'change .selector' : 'select',
-	    'click .editor'    : 'edit',
-	    'submit'           : 'save',
-	    'click .cancel'    : 'render'
-	},
-	
-	initialize: function(){
-	    _.bindAll(this);
-	    vent.on('changeParty', this.select);
-	    vent.on('createdParty', this.creationEdit);
-	    vent.on('detach', this.remove);
-	    this.listenTo(this.model, 'invalid', this.notifyValidity);
-	    this.render();
-	},
-
-	changeModel: function(newModel) {
-	    this.stopListening(this.model);
-	    this.model = newModel;
-	    this.listenTo(this.model, 'invalid', this.notifyValidity);
-	},
-
-	notifyValidity: function() {
-	    var message = {
-		title: "Party edit validation",
-		text: "Input is invalid, " + this.model.validationError
-	    };
-	    vent.trigger('alert', message);
-	},
-	
-	render: function(){
-	    this.$el.html(party_description({party: this.model.toJSON(), editable: true}));
-	    return this;
-	},
-
-	select: function(partyId) {
-	    this.model = parties.get(partyId);
-	    this.render();
-	},
-
-	edit: function() {
-	    this.$el.html(party_edit({party:this.model.toJSON()}));
-	    $('.datepicker', this.$el).datepicker({
-		format: "dd.mm.yyyy",
-		startDate: new Date(),
-		autoclose: true
-	    });
-	    $('.timepicker', this.$el).timepicker({
-		showMeridian: false,
-		showSeconds: false,
-		minuteStep: 30
-	    });
-	},
-
-	creationEdit: function(partyId){
-	    this.changeModel(parties.get(partyId));
-	    this.edit();
-	},
-	
-	//todo move to time/date parsing module
-	parseData: function(data) {
-	    var parseDate = function(dateString, timeString) {
-		var date = moment(dateString, "DD.MM.YYYY");
-		var time = timeString.split(":");
-		date.hours(time[0]);
-		date.minutes(time[1]);
-		return date.toDate();
-	    };
-
-	    var parsed = {};
-	    parsed.title = data.title;
-	    parsed.description = data.description;
-	    parsed.date = parseDate(data.date, data.startTime);
-	    parsed.infoDate = parseDate(data.date, data.infoTime);
-	    
-	    return parsed;
-	},
-
-	save: function() {
-	    var arr = this.$("#edit_party").serializeArray();
-	    var input_data = _(arr).reduce(function(acc, field) {
-		acc[field.name] = field.value;
-		return acc;
-	    }, {});
-	    var data = this.parseData(input_data);
-	    this.model.save(data, {
-		success: this.notify, 
-		error: this.alert,
-		wait:true
-	    });
-	    return false;
-	},
-
-	notify: function(model, options) {
-	    var message = {
-		title: 'Success',
-		text: "Party " + model.get('title') + " successfully modified."
-	    };
-	    vent.trigger('notify', message);
-	    vent.trigger('partyEdited', model);
-	    this.render();
-	},
-
-	alert: function(model, xhr, options) {
-	    var message = {
-		title: "Failure in Party modification "+ model.get('title') + " (Something went wrong in server)!",
-		text: "Your request failed because: " + xhr.responseText
-	    };
-	    vent.trigger('alert', message);
-	}
-    });
 
     var initialize = function(options) {
 	var latestParty;
@@ -350,7 +236,7 @@ define([
 		    
 		    var _ready = _.after(3, function(){
 			partySelector.createComponent({el: $('#party-selector',rootel), collection: parties, model: latestParty}, vent);
-			new Party_Viewer({el: $('#party', rootel), model: latestParty});
+			partyEditor.createComponent({el: $('#party', rootel), collection: parties, model: latestParty}, vent);
 			new Nakki_List({el: $('#nakit', rootel), model: latestParty, collection: nakkitypes});
 
 			usersList.createConstructors({el: $('#constructors', rootel), collection: auxUsers}, vent);
@@ -365,7 +251,7 @@ define([
 		    nakkitypes.fetch({success: _ready, error: _error});
 		} else { //TODO remove?
 		    partySelector.createComponent({el: $('#party-selector',rootel), collection: parties}, vent);
-		    new Party_Viewer({el:$('#party',rootel), model: new models.Party({title:'No parties yet'})});
+		    partyEditor.createComponent({el:$('#party',rootel), collection: parties, model: new models.Party({title:'No parties yet'})}, vent);
 		    new Nakki_List({el:$('#nakit',rootel), model: latestParty, collection: nakkitypes});
 
 		    usersList.createUsers({el: $('#users', rootel), collection: users}, vent);
