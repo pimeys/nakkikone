7975111bbe7735a3b1b7b53ff049de1c6cabec81
Sami Airaksinen <samiaira@gmail.com>
2013-10-06 13:55:43 +0300

    Extracted aux job components from public view.

---
+++ app/assets/javascripts/components/aux-jobs.js
@@ -0,0 +1,116 @@
+"use strict";
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'collections',
+    'models',
+    'bs',
+    'hbs!templates/aux-job-selector',
+    'hbs!templates/aux-job-counter'
+], function($, _, bb, collections, models, bs, auxJobSelector, auxjobList) {
+
+    var vent;
+
+    var AuxJob = models.PartyResource.extend({
+	resource: 'aux_nakit'
+    });
+
+    // horrible piece of shit, please die.
+    var AuxJobsSelect = bb.View.extend({
+	initialize: function() {
+	    _.bindAll(this);
+	    this.listenTo(vent, 'assignPerson', this.submit);
+	    this.listenTo(vent, 'detach', this.detach);
+	    this.render();
+	},
+
+	detach: function() {
+	    this.stopListening();
+	    this.remove();
+	},
+
+	createSingleUseModel: function() {
+	    var model = new AuxJob();
+	    model.partyId = this.model.get('id');
+	    return model;
+	},
+
+	submit: function(assigned) {
+	    var type = this.$('form').serializeArray()[0].value;
+	    if (type === "both") {
+	    	this.createSingleUseModel().save({type:"clean"}, {wait:true, success:this.notify, error: this.alert});
+		this.createSingleUseModel().save({type:"const"}, {wait:true, success:this.notify, error: this.alert});
+	    } else if (type === "none"){
+		return false;
+	    } else {
+		this.createSingleUseModel().save({type:type}, {wait:true, success: this.notify, error: this.alert});
+	    }
+	    return false;
+	},
+
+	notify: function(model, options) {
+	    var message = {
+		title: "Success!",
+		text: "Your " + model.get('type') + " has been succesfully registered for you."
+	    };
+	    vent.trigger('notify', message);
+	},
+
+	alert: function(model, xhr, options) {
+	    var message = {
+		title: "Failure (Something went wrong in server)!",
+		text: "Your assignment request failed because: " + xhr.responseText
+	    };
+	    vent.trigger('alert', message);
+	},
+
+	render: function(){
+	    this.$el.html(auxJobSelector());
+	    return this;
+	}
+    });
+
+    var AuxJobList = bb.View.extend({
+	titleForList: "ovrride this",
+	filterRules: {type: "none"},
+
+	initialize: function(){
+	    _.bindAll(this);
+	    this.listenTo(vent, 'detach', this.detach);
+	    this.listenTo(this.collection, 'reset add destroy remove', this.render);
+	    this.render();
+	},
+
+	render: function(){
+	    var filtered = new bb.Collection().add(this.collection.where(this.filterRules));
+	    this.$el.html(auxjobList({name: this.titleForList ,count: filtered.size(), auxJobs: filtered.toJSON()}));
+	}
+    });
+
+    var CleanJobList = AuxJobList.extend({
+	titleForList: "Number of Taking it Down:",
+	filterRules: {type: "clean"}
+    });
+
+    var ConstJobList = AuxJobList.extend({
+	titleForList: "Number of Builders:",
+	filterRules: {type: "const"}
+    });
+
+    return {
+	createSelector: function(options, _vent) {
+	    vent = _vent; //todo remvoe
+	    return new AuxJobsSelect(options);
+	},
+	
+	createCleanersList: function(options, _vent) {
+	    vent = _vent; //todo remvoe
+	    return new CleanJobList(options);
+	},
+	createConstructorsList: function(options, _vent) {
+	    vent = _vent; //todo remvoe
+	    return new ConstJobList(options);
+	}
+    };
+});
--- app/assets/javascripts/views/public.js
+++ app/assets/javascripts/views/public.js
@@ -8,80 +8,16 @@ define([
     'components/party-viewer',
     'components/nakki-table',
     'components/notification-area',
+    'components/aux-jobs',
     'bs',
-    'hbs!templates/public-screen',
-    'hbs!templates/aux-job-selector',
-    'hbs!templates/aux-job-counter'
-], function($, _, bb, collections, models, partyViewer, nakkiTable, notificationArea, bs, publicScreen, auxJobSelector, auxjobList) {
+    'hbs!templates/public-screen'
+], function($, _, bb, collections, models, partyViewer, nakkiTable, notificationArea, auxJobs, bs, publicScreen) {
 
     var vent = {};
     _.extend(vent, bb.Events);
 
-    var AuxJob = models.PartyResource.extend({
-	resource: 'aux_nakit'
-    });
-
     var party = new models.Party();
     var nakit = new collections.Nakit();
-    var AuxUsers = collections.AuxUsers.extend({
-	resource: 'aux_parcipitants_names'
-    });
-    var auxUsers = new AuxUsers();
-
-    // horrible piece of shit, please die.
-    var AuxJobsSelect = bb.View.extend({
-	initialize: function() {
-	    _.bindAll(this);
-	    this.listenTo(vent, 'assignPerson', this.submit);
-	    this.listenTo(vent, 'detach', this.detach);
-	    this.render();
-	},
-
-	detach: function() {
-	    this.stopListening();
-	    this.remove();
-	},
-
-	createSingleUseModel: function() {
-	    var model = new AuxJob();
-	    model.partyId = party.get('id');
-	    return model;
-	},
-
-	submit: function(assigned) {
-	    var type = this.$('form').serializeArray()[0].value;
-	    if (type === "both") {
-	    	this.createSingleUseModel().save({type:"clean"}, {wait:true, success:this.notify, error: this.alert});
-		this.createSingleUseModel().save({type:"const"}, {wait:true, success:this.notify, error: this.alert});
-	    } else if (type === "none"){
-		return false;
-	    } else {
-		this.createSingleUseModel().save({type:type}, {wait:true, success: this.notify, error: this.alert});
-	    }
-	    return false;
-	},
-
-	notify: function(model, options) {
-	    var message = {
-		title: "Success!",
-		text: "Your " + model.get('type') + " has been succesfully registered for you."
-	    };
-	    vent.trigger('notify', message);
-	},
-
-	alert: function(model, xhr, options) {
-	    var message = {
-		title: "Failure (Something went wrong in server)!",
-		text: "Your assignment request failed because: " + xhr.responseText
-	    };
-	    vent.trigger('alert', message);
-	},
-
-	render: function(){
-	    this.$el.html(auxJobSelector());
-	    return this;
-	}
-    });
 
     var Assign_Form = bb.View.extend({
 	events: {
@@ -117,34 +53,14 @@ define([
 	}
     });
 
-    var AuxJobList = bb.View.extend({
-	titleForList: "ovrride this",
-	filterRules: {type: "none"},
-
-	initialize: function(){
-	    _.bindAll(this);
-	    this.render();
-	},
-
-	render: function(){
-	    var filtered = new bb.Collection().add(this.collection.where(this.filterRules));
-	    this.$el.html(auxjobList({name: this.titleForList ,count: filtered.size(), auxJobs: filtered.toJSON()}));
-	}
-    });
-
-    var CleanJobList = AuxJobList.extend({
-	titleForList: "Number of Taking it Down:",
-	filterRules: {type: "clean"}
-    });
-
-    var ConstJobList = AuxJobList.extend({
-	titleForList: "Number of Builders:",
-	filterRules: {type: "const"}
-    });
-
     //todo move to separate error-handling-module
     var _error = function(col, error) {
     };
+
+    var AuxUsers = collections.AuxUsers.extend({
+	resource: 'aux_parcipitants_names'
+    });
+    var auxUsers = new AuxUsers();
 
     var initialize = function(options) {
 	var rootel = options.el;
@@ -171,10 +87,9 @@ define([
 		nakkiTable.createComponent({el:$('#nakki-table',rootel), collection: nakit}, vent);
 		new Assign_Form({el:$('#assign',rootel), model: options.currentUser()});
 
-		//todo refactor to oblivion
-		var auxjobs = new AuxJobsSelect({el: $('#auxJob-selector', rootel)});
-		new CleanJobList({el: $('#auxJob-cleaners', rootel), collection: auxUsers});
-		new ConstJobList({el: $('#auxJob-constructors', rootel), collection: auxUsers});
+		auxJobs.createSelector({el: $('#auxJob-selector', rootel), model: party}, vent);
+		auxJobs.createCleanersList({el: $('#auxJob-cleaners', rootel), collection: auxUsers}, vent);
+		auxJobs.createConstructorsList({el: $('#auxJob-constructors', rootel), collection: auxUsers}, vent);
 	    });
 
 	    nakit.fetch({success: _ready, error: _error});
