f13662dc36496d833f6bc4591feefd780ba76735
Sami Airaksinen <samiaira@gmail.com>
2015-04-19 12:18:56 +0300

    Building and cleaning are now checkboxes
    - improves usability

--- app/assets/javascripts/views/public.js
+++ app/assets/javascripts/views/public.js
@@ -62,7 +62,7 @@ define([
 		nakkiTable.createComponent({el:$('#nakki-table',rootel), collection: nakit }, vent);
 		assignForm.createComponent({el:$('#assign',rootel), model: options.currentUser()}, vent, party);
 
-		auxJobs.createSelector({el: $('#auxJob-selector', rootel), model: party, collection: auxUsers}, vent);
+		auxJobs.createSelector({el: $('#auxJob-selector', rootel), model: party, collection: auxUsers, user: options.currentUser()}, vent);
 		auxJobs.createCleanersList({el: $('#auxJob-cleaners', rootel), collection: auxUsers}, vent);
 		auxJobs.createConstructorsList({el: $('#auxJob-constructors', rootel), collection: auxUsers}, vent);
 	    });
